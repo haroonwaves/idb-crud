b3ef6ab234fb4784d7d43da4b4fc2b57121f3173
Usman Haroon <haroonusman00@gmail.com>
2024-02-15 12:17:39 +0530

    ✨Basic table

--- src/components/Main/Table/table.jsx
+++ src/components/Main/Table/table.jsx
@@ -1,5 +1,109 @@
-const IdbCrudTable = () => {
-  return <div>I am the table</div>;
-};
+import { useState } from "preact/hooks";
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import "./Styles/table.scss";
+
+const defaultData = [
+  {
+    firstName: "tanner",
+    lastName: "linsley",
+    age: 24,
+    visits: 100,
+    status: "In Relationship",
+    progress: 50,
+  },
+  {
+    firstName: "tandy",
+    lastName: "miller",
+    age: 40,
+    visits: 40,
+    status: "Single",
+    progress: 80,
+  },
+  {
+    firstName: "joe",
+    lastName: "dirte",
+    age: 45,
+    visits: 20,
+    status: "Complicated",
+    progress: 10,
+  },
+];
+
+const columnHelper = createColumnHelper();
+
+const columns = [
+  columnHelper.accessor("firstName", {
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor((row) => row.lastName, {
+    id: "lastName",
+    cell: (info) => <i>{info.getValue()}</i>,
+    header: () => <span>Last Name</span>,
+  }),
+  columnHelper.accessor("age", {
+    header: () => "Age",
+    cell: (info) => info.renderValue(),
+  }),
+  columnHelper.accessor("visits", {
+    header: () => <span>Visits</span>,
+  }),
+  columnHelper.accessor("status", {
+    header: "Status",
+  }),
+  columnHelper.accessor("progress", {
+    header: "Profile Progress",
+  }),
+];
+
+function IdbCrudTable() {
+  const [data, setData] = useState(() => [...defaultData]);
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div>
+      <table className="idb-crud-table">
+        <thead className="idb-crud-table-header">
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr className="idb-crud-table-row" key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th className="idb-crud-table-head" key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody className="idb-crud-table-body">
+          {table.getRowModel().rows.map((row) => (
+            <tr className="idb-crud-table-row" key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td className="idb-crud-table-data" key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
 
 export default IdbCrudTable;
