d8d3c04fb01adf5448a1ef8fb2a2883babe607da
Usman Haroon <haroonusman00@gmail.com>
2024-02-22 19:51:07 +0530

    ✨ Implement tailwind

--- src/content.jsx
+++ src/content.jsx
@@ -1,10 +1,19 @@
 import { render } from "preact";
 import { App } from "../src/app";
 
+import appStyle from "../src/styles/app.scss?inline";
+import tailwind from "./index.css?inline";
+
 const contentRoot = document.createElement("div");
 contentRoot.id = "idb-crud-content-root";
+
+const style = document.createElement("style");
+style.innerHTML = `${tailwind} ${appStyle}`;
+
 document.body.append(contentRoot);
 
 const shadowRoot = contentRoot.attachShadow({ mode: "open" });
+
+shadowRoot.appendChild(style);
 
 render(<App />, shadowRoot);
