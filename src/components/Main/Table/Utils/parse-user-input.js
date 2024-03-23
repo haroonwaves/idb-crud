0fc473a0a83c53622b66091f1d3b6dd2f9909677
Usman Haroon <haroonusman00@gmail.com>
2024-03-23 18:57:37 +0530

    🐛 Fix parse number
    * Validate number with regular expression before parse it.

--- src/components/Main/Table/Utils/parse-user-input.js
+++ src/components/Main/Table/Utils/parse-user-input.js
@@ -7,9 +7,13 @@ export function parseUserInput(input) {
     return undefined;
   }
 
-  const number = parseFloat(input);
-  if (!isNaN(number)) {
-    return number; // Return as number if it's a valid number
+  // Adding a regex to better validate numeric values, including integers, floats, and scientific notation
+  const isNumeric = /^-?\d+(\.\d+)?(e[+-]?\d+)?$/i;
+  if (isNumeric.test(input)) {
+    const number = parseFloat(input);
+    if (!isNaN(number)) {
+      return number; // Return as number if it's a valid number
+    }
   }
 
   if (input === "true") {
