fee0a303ebc10f2b9646bd8ca574d4f75253468f
Usman Haroon <haroonusman00@gmail.com>
2024-05-01 11:04:25 +0530

    🐛 Fix scrolling main site
    * We used to set a body's overflow property to auto, it prevents scrolling on some sites ex: facebook

--- src/content.jsx
+++ src/content.jsx
@@ -3,17 +3,21 @@ import { App } from "../src/app";
 
 import tailwind from "./index.css?inline";
 import appStyles from "./styles/app.scss?inline";
+import hostStyles from "./styles/host.scss?inline";
 
 const contentRoot = document.createElement("div");
 contentRoot.id = "idb-crud-content-root";
 
-const style = document.createElement("style");
-style.innerHTML = `${tailwind} ${appStyles}`;
+const hostStyle = document.createElement("style");
+hostStyle.innerHTML = `${hostStyles}`;
 
-document.body.append(contentRoot);
+const extensionStyle = document.createElement("style");
+extensionStyle.innerHTML = `${tailwind} ${appStyles}`;
+
+document.body.append(contentRoot, hostStyle);
 
 const shadowRoot = contentRoot.attachShadow({ mode: "open" });
 
-shadowRoot.append(style);
+shadowRoot.append(extensionStyle);
 
 render(<App />, shadowRoot);
