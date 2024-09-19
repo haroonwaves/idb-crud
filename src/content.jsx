9db1abbb202e4d4ef0c6e6a5f326f6edc0e329ad
Usman Haroon <haroonusman00@gmail.com>
2024-09-19 15:03:38 +0530

    ✨ Download archivo font

--- src/content.jsx
+++ src/content.jsx
@@ -5,6 +5,12 @@ import tailwind from "./index.css?inline";
 import appStyles from "./styles/app.scss?inline";
 import hostStyles from "./styles/host.scss?inline";
 
+const fontLink = document.createElement("link");
+fontLink.href =
+  "https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap";
+fontLink.rel = "stylesheet";
+document.head.appendChild(fontLink);
+
 const contentRoot = document.createElement("div");
 contentRoot.id = "idb-crud-content-root";
 
