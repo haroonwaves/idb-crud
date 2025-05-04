a95dd84d5309fcd2d83593b3b686cece9a2ecdde
Usman Haroon <haroonusman00@gmail.com>
2025-05-04 17:53:31 +0530

    🗑️ chore: remove deprecated components and styles from the old codebase to streamline the project structure

--- src_old/Utils/get-selected-values.js
+++
@@ -1,9 +0,0 @@
-import appState from "../AppState/appSate";
-
-export default function getSelectedValues() {
-  const selectedRows = appState.selectedRows.value;
-  const tableDate = appState.tableData.value;
-  const selectedValues = selectedRows.map((index) => tableDate[index]);
-
-  return selectedValues;
-}
