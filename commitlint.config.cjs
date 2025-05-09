714b06d8dc9510a7ef36dc5fd61a02e117575a63
Usman Haroon <haroonusman00@gmail.com>
2025-05-09 11:08:31 +0530

    ✨ feat: add commitlint configuration and husky commit message validation to enforce commit message format

---
+++ commitlint.config.cjs
@@ -0,0 +1,43 @@
+module.exports = {
+	parserPreset: {
+		parserOpts: {
+			headerPattern: /^(\p{Emoji_Presentation}|\p{Extended_Pictographic})\s([a-z]+):\s(.+)$/u,
+			headerCorrespondence: ['emoji', 'type', 'subject'],
+		},
+	},
+	rules: {
+		'type-enum': [
+			2,
+			'always',
+			[
+				'feat',
+				'fix',
+				'docs',
+				'style',
+				'refactor',
+				'perf',
+				'test',
+				'chore',
+				'revert',
+				'build',
+				'ci',
+				'deps',
+				'dx',
+				'ui',
+				'config',
+				'init',
+				'security',
+				'i18n',
+				'a11y',
+				'seo',
+				'types',
+				'db',
+				'api',
+				'auth',
+				'lint',
+			],
+		],
+		'subject-empty': [2, 'never'],
+		'type-case': [2, 'always', 'lower-case'],
+	},
+};
